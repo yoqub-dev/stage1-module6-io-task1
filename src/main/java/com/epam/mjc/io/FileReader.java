8642e1ad354c086f092d2db19bdf61de9422691b
yoqub-dev <yoqub.epam@gmail.com>
2023-11-05 04:49:45 +0500

    task3

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -44,6 +44,8 @@ public class FileReader {
                     }
                 }
             }
+            return new Profile(name, age, email, phone);
+
         } catch (IOException e) {
             logger.severe(e.getMessage());
         }
