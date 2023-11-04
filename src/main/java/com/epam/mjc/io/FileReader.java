5d4ea94f4620f6883fee3c4b54a7c6330a7bdd4b
yoqub-dev <yoqub.epam@gmail.com>
2023-11-05 04:09:12 +0500

    task3

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new java.io.FileReader(file));
+        String line;
+        String name = null;
+        Integer age = null;
+        String email = null;
+        Long phone = null;
+        while ((line = reader.readLine()) != null) {
+            String[] keyValue = line.split(": ");
+            if (keyValue.length == 2) {
+                String key = keyValue[0].trim();
+                String value = keyValue[1].trim();
+
+                if ("Name".equals(key)) {
+                    name = value;
+                } else if ("Age".equals(key)) {
+                    age = Integer.parseInt(value);
+                } else if ("Email".equals(key)) {
+                    email = value;
+                } else if ("Phone".equals(key)) {
+                    phone = Long.parseLong(value);
+                }
+            }
+        }
+
+        if (name != null && age != null && email != null && phone != null) {
+            return new Profile(name, age, email, phone);
+        } else {
+            throw new IllegalArgumentException("Invalid data in the file");
+        }
     }
 }
